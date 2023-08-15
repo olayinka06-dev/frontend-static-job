7144e4b21a6e7f9d68217f19794fadc43f751910
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-08-15 08:35:55 -0700

    completing the project thou adjustment may still be needed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ export default function Home () {
       </main>
       <div className="flex flex-col text-main gap-2 py-2 text-center">
         <span>Built with Love By <span className="text-[red]">Olayinka_Dev</span></span>
-        <span>Follow and Give it a Star on <button class="inline-flex text-white bg-main border-0 py-2 px-6 focus:outline-none hover:opacity-[70%] rounded text-lg">
+        <span>Follow and Give it a Star on <button className="inline-flex text-white bg-main border-0 py-2 px-6 focus:outline-none hover:opacity-[70%] rounded text-lg">
             GitHub
         </button></span>
       </div>
